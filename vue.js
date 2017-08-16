e7e324cedc318f5ab3dd9e6fe4268e4a64a0e4fd
davidblj <david.blj95@gmail.com>
2017-08-15 19:14:06 -0500

    Components part 2

--- vue.js
+++ vue.js
@@ -1,8 +1,27 @@
 Vue.component('task', {
 
-    // the "slot" tag, takes in any text that is typed in the html file to be displayed as
-    // text in our list item
     template: '<li><slot></slot></li>'
+});
+
+Vue.component('task-list', {
+
+    // templates must have a root element. Thats why we enclose a "task" within "div"
+    template: `<div><task v-for="task in tasks">{{task.description}}</task></div>`,
+
+    // we can't bind data directly to the data variable. We do so through a method
+    data() {
+        return {
+
+            tasks: [
+                {description: 'Go the store', completed: true},
+                {description: 'Finish screencast', completed: false},
+                {description: 'make donation', completed: false},
+                {description: 'Clear inbox', completed: false},
+                {description: 'make dinner', completed: false},
+                {description: 'clean room', completed: true},
+            ]
+        };
+    }
 });
 
 new Vue({
