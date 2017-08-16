501f2804feee42aa6902f18d46df8b770e50d750
davidblj <david.blj95@gmail.com>
2017-08-15 23:56:38 -0500

    Recap

--- vue.js
+++ vue.js
@@ -1,17 +1,85 @@
-Vue.component('modal', {
+Vue.component('tabs', {
 
     template: `
-    <div class="modal  is-active">
-            <div class="modal-background"></div>
-            <div class="modal-content">
 
-                <div class="box">
-                    <slot></slot>
-                </div>
+        <div>
+             <div class="tabs">
+                <ul>
+                   <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive}">
+                   <a :href="tab.href" @click="selectTab(tab)"> {{tab.name}}</li>
+                </ul>
             </div>
-            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+            
+            <div class="tabs-details">
+                <slot></slot>
+            </div>         
         </div>
-    `
+       
+    `,
+
+    mounted() {
+        // list all the child components inside this component:  3 tab elements.
+        // note that for this line to work, we have to define a "tab" component (1)
+        console.log(this.$children);
+    },
+
+    data() {
+
+        // this variable must be initialized as an array
+        return { tabs: [] };
+    },
+
+    // return all tabs in this variable, so we can reference the dynamically in our Bulma's tab class
+    created() {
+        this.tabs = this.$children;
+    },
+
+    // now we need to highlight the active tab. We do this by setting "tab.isActive = true" to the
+    // clicked tab and "false" otherwise. We cant change the "selected" value as instructed by Vue,
+    // and so that's why we created an additional variable
+
+    methods: {
+
+        selectTab(selectedTab) {
+
+            // tab.isActive is also useful to show its contents
+            this.tabs.forEach( tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            });
+        }
+    }
+
+});
+
+// (1) We can now filter each tab with its corresponding name:
+// $vm0.$children.forEach( tab => console.log(tab.name))
+Vue.component('tab', {
+    template: `
+        <div v-show="isActive"><slot></slot></div>
+    `,
+
+    props: {
+        name: {required: true},
+        selected: { default: false}
+    },
+
+    data() {
+
+        return { isActive: false };
+
+    },
+
+    mounted() {
+        this.isActive = this.selected;
+    },
+
+    computed: {
+        href()  {
+
+            // about-us
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    }
 
 });
 
@@ -19,7 +87,6 @@ Vue.component('modal', {
 // our components. For that we define a custom event with the syntax $emit('nameOfTheCustomEvent').
 
 // note that "is-active" class is always on. We control its visibility with Vuejs
-
 new Vue({
 
     el: '#root',
