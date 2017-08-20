285b76f8855849082531f544c0d6ae9999b75048
davidblj <david.blj95@gmail.com>
2017-08-20 15:58:52 -0500

    Vue-cli

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <message>Hello There</message>
+        <message>How is it going ?</message>
+    </div>
+</template>
+
+<script>
+
+    // we need to compile everything in a file so that the web browser can understand
+    // and thats why we use webpack. Take a look at webpack.config.js
+
+    // Along the way, we are going to create a component named Message.vue in the
+    // folder src/components. for that, we need to define a components field in our main .vue file
+    import Message from './components/Message.vue'
+
+    export default {
+        name: 'app',
+
+        components: { Message },
+
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box">
+        <p><slot></slot></p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .box {
+        background: darkslategray  ;
+        padding: 10px;
+        border: 1px solid azure;
+        color: white;
+    }
+</style>
