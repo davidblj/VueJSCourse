3d42d5b9bba426d35e24ab414e69acae787e0681
davidblj <david.blj95@gmail.com>
2017-08-20 14:21:37 -0500

    Inline Templates

--- vue.js
+++ vue.js
@@ -1,33 +1,11 @@
-Vue.component('modal',{
+Vue.component('progress-view',{
 
-    template: `
-    <div class="modal is-active">
-    <div class="modal-background"></div>
-    <div class="modal-card">
-    
-        <header class="modal-card-head">
-          <p class="modal-card-title">
-            
-            <slot name="header"></slot>
-            
-          </p>
-          <button class="delete" aria-label="close"></button>                      
-        </header>
-        
-        <section class="modal-card-body">         
-           <slot> 
-               Default content that can be overriden if any text is placed on the
-               html 
-           </slot>
-        </section>
-        
-        <footer class="modal-card-foot">
-            <slot name="footer"></slot>
-        </footer>
-    
-    </div>
-    </div>
-    `
+    data() {
+        return {
+            completionRate: 50
+        };
+    }
+
 });
 
 new Vue({
