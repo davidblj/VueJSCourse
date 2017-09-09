5f0c4617eaf3f3eda9fae242cc1848e82d7f7f94
davidblj <david.blj95@gmail.com>
2017-09-09 12:35:47 -0500

    Scoped slots

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,55 @@
 <template>
     <div id="app">
-        <candidato v-for="candidato in candidatos"
-                   :nombre="candidato.name.first"
-                   :correoe="candidato.email"
-                   :imagen="candidato.picture.thumbnail"
-        >
-        </candidato>
+        <tareas :listado="tareas">
+            <template slot="tarea" scope="datos">
+                <li> <h1>{{ datos.title }}</h1></li>
+            </template>
+        </tareas>
+
+        <tareas :listado="tareas">
+            <template slot="tarea" scope="datos">
+                <li>{{ datos.title }}</li>
+            </template>
+        </tareas>
+
+        <tareas :listado="tareas">
+            <template slot="tarea" scope="datos">
+                <li>🐱{{ datos.title }}</li>
+            </template>
+        </tareas>
     </div>
 </template>
 
 <script>
 
-    // we will see now how we can pass data between a parent and its child through props
+    // we will dive deeper in to named slots and properties. Scoped slots gives a
+    // great advantage
 
-    // todo: Fix axios request. In order for this to work, you need to make a change to reload our app.
-
-    import candidato  from './components/candidato.vue'
+    import tareas  from './components/tareas.vue'
 
     export default {
         name: 'app',
 
-        components: { candidato },
+        components: { tareas },
 
-        mounted() {
-            this.obtenerCandidatos();
-        },
+        // we will access each item (title) in "tareas.vue". For that, we define a scope (datos).
+        // that variable can now access the property "title". That means that datos.title will
+        // generate a new entry in our html depending of the quantity of items in :title.
+
+        // datos can access any property in a similar manner. it doesnt have to reference a variable
+        // inside a v-for. And it also might change depending on the slot name
+
+        // We can now have an instance on this component with different styles that ARE NOT defined in
+        // the original component
 
         data() {
-            return { candidatos: []}
-        },
-
-        methods: {
-            obtenerCandidatos() {
-                axios.get('https://randomuser.me/api/?results=100')
-                    .then((respuesta) => {
-                        this.candidatos = respuesta.data.results;
-                    });
-            }
+            return { tareas: [
+                { titulo: 'Salir a correr'},
+                { titulo: 'Recoger la casa'},
+                { titulo: 'Aprender Vue.js'},
+                { titulo: 'Ir al gimnasio'},
+                { titulo: 'Leer cada día'},
+            ]}
         }
 
         }
---
+++ src/components/tareas.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+     <ul>
+         <ul>
+             <slot name="tarea" v-for="tarea in listado" :title="tarea.titulo"></slot>
+             <!--        <li v-for="tarea in listado">
+                         {{ tarea.titulo }}
+                     </li>-->
+         </ul>
+     </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props: ['listado']
+    }
+</script>
+
+<style lang="scss">
+</style>
