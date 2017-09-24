8ba5a92b96302b835cb24fd5e1ce04d51a67fb8b
davidblj <david.blj95@gmail.com>
2017-09-24 07:30:42 -0500

    States

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <template>
     <div id="app">
-       <h1 :class="[$style.rojo]">Hello world</h1>
-        <h1 :class="{[$style.subrayado]: subrayado}">Hello world</h1>
+
     </div>
 </template>
-
 <script>
     export default {
         name: 'app',
-        data() {
-            return {
-                subrayado: true     // you can down the road, change the value of this variable
-                                    // for dynamic style rules
-            }
-        }
-
-        // like scoped variables, we can define local rules. These rules are unique within
-        // the component because webpack assign them a coded name. You can inspect the generated HTML.
-
         }
 </script>
+<style lang="scss">
 
-<style module lang="scss">
-
-    .subrayado {
-        text-decoration: underline;
-    }
-    .rojo {
-        color: red
-    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+const compartido = {
+  usuario: {
+    nombre: 'David'
+  }
+};
+
+// we have seen how we can share variables within components through 'props' and events.  States are different. If i change
+// the constant define here, all the components using this state, will also change their values.
+// try issuing the command: $vm0.usuario.nombre = 'jaramillo'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  data: compartido
 });
 
+new Vue({
+    el: '#app2',
+    data: compartido
+});
