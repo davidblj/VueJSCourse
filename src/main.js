b286cc8e9c5030e6f13ec44bf7389cfbb2e2fbb5
davidblj <david.blj95@gmail.com>
2017-10-15 21:54:22 -0500

    Vue Router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import {store} from './store/store'
+const home = Vue.component('Home', {
+    template: '<h2>Portada</h2>',
+});
+
+const contacto = Vue.component('Contacto', {
+    template: '<h2>Contacto</h2>',
+});
+
+const routes = [
+    {path: '/', component: home},
+    {path: '/contacto', component: contacto},
+];
+
+const router = new VueRouter({
+    routes: routes,
+});
 
 new Vue({
-    el: '#app',
-    store,  // store: store,
-    render: h => h(App)
+    router: router,
+    el: 'main',
 });
